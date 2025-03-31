e666500b5743d0ca0d2caacfc2c882978759fa88
Hanju Kim <hanjkim@c3r3p4.42vienna.com>
2025-03-31 14:53:02 +0200

    lol2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hanjkim <hanjkim@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 14:04:44 by hanjkim           #+#    #+#             */
-/*   Updated: 2025/03/31 14:45:48 by hanjkim          ###   ########.fr       */
+/*   Updated: 2025/03/31 14:52:53 by hanjkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ int main(int argc, char **argv)
 {
 	t_data data;
 
-	ft_set_up_game(&data);
+	/*ft_set_up_game(&data);*/
 	while (1)
 	{
 		// magic happens here
 	}
-	exit(0);
+	if (argv || argc)
+		exit(0);
+	else
+		exit(1);
 }
